b664ddd0f3b89992163c977e032a1f1918afb747
Swino4ka <sasakvartuk@gmail.com>
2024-04-07 00:50:23 +0200

    Did all things

---
+++ hello-world/src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+
+  <div class="header__block">    
+    <img class="header__icon" width="44px" height="44px" src="../assets/icon.svg" alt="">
+    <img class="header__text" width="174px" height="36px" src="../assets/Math.svg" alt="">
+  </div>
+  
+  </header>
+</template>
+
+<script>
+import "@/css/components/header.css";
+
+export default {
+  name: 'HeaderComponent',
+}
+</script>
