d0cf78c574d648291ed8ecabcf9f2e0f4ef236cf
Madara0330E <summoners2011sss@gmail.com>
2024-04-20 21:07:55 +0500

    фывфвasdadsad
    ыффasdada

--- quiz-game/src/components/HomePage.vue
+++ quiz-game/src/components/HomePage.vue
@@ -14,10 +14,12 @@
   </div>
 </div>
 <div class="RegisterDiscordMeneger">
-<button class="RegisterDiscord">
-  <img src="../img/home/Discord.png" alt="">
-  <p>Sigh in with Discord</p>
-</button>
+  <button class="RegisterDiscord">
+    <a href="https://discord.com/oauth2/authorize?client_id=1227686332260946000&response_type=code&redirect_uri=https%3A%2F%2Fquiz-game-three-cyan.vercel.app%2F&scope=identify" target="_blank">
+      <img src="../img/home/Discord.png" alt="">
+      <p>Sign in with Discord</p>
+    </a>
+  </button>
 </div>
 <div class="MadeTextMeneger">
 <div class="Madetext">
@@ -37,6 +39,22 @@ export default {
     msg: String
   }
 }
+// Функция для установки куки
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Предположим, что после успешной регистрации через Discord у вас есть токен доступа
+var discordAccessToken = "your_access_token_here";
+
+// Устанавливаем куки с токеном доступа на, скажем, 30 дней
+setCookie("discord_access_token", discordAccessToken, 30);
 </script>
 <style >
 body {
